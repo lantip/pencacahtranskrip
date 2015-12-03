118ebef03146a5d3dfbffcc030188acd82a77ca5
Lantip <lantip@gmail.com>
2015-12-03 21:19:54 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+PencacahKata
+=======
+
+Berhubung sedang ramai membicarakan transkrip rekaman `papa minta saham`, dan di beberapa media juga memunculkan headlines tentang sejumlah nama yang disebut, maka saya bikinkan script ini untuk menghitungkan.
+
+
+Penggunaan
+----------
+
+gampang saja sih, masukkan nama atau kata yang ingin dicari, lalu klik enter
+
+```
+    misal: python counter.py -i luhut
+    hasil: Counter({'luhut': 65})
+```
+
+
+Kredit
+------
+Data transkrip diambil dari [GoogleDoc](https://drive.google.com/file/d/0BxNJIO5pfNYsemxINno3S1FxUUE/view)
+
---
+++ counter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys, getopt
+from collections import Counter
+import re
+
+def main(argv):
+    findtext = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:",["text="])
+    except getopt.GetoptError:
+        print 'counter.py -i <findtext>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'counter.py -i <findtext>'
+            sys.exit()
+        elif opt in ("-i", "--text"):
+            findtext = arg
+    words = re.findall(r'\w+', open('transkrip.txt').read().lower())
+    counts = Counter()
+    for word in words:
+        if word == findtext:
+            counts[word] += 1
+    print counts
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
